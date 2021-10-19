fef54cab0e3082bb4c6dbc18accb60947686d801
TMineCola <23163771+TMineCola@users.noreply.github.com>
2021-10-19 17:44:39 +0800

    Added Space to 4

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const { router, line } = require('bottender/router');
 const messageRouter = require('./routes/bot/messageRouter');
 
 async function App(context) {
-  return router([
-    line.message(messageRouter),
-    line.postback(HandlePostback),
-    line.any(HandleLine)
-  ]);
+    return router([
+        line.message(messageRouter),
+        line.postback(HandlePostback),
+        line.any(HandleLine)
+    ]);
 };
 
 async function HandlePostback(context) {
-  console.log(context);
+    console.log(context);
 }
 async function HandleLine(context) {
-  console.log(context);
+    console.log(context);
 }
 
 module.exports = App;
