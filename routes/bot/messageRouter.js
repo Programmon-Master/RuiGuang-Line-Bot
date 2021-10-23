102d75dc6e0cae738f454d42addac746e509dc44
TMineCola <23163771+TMineCola@users.noreply.github.com>
2021-10-24 00:43:45 +0800

    Added send back activity router

--- routes/bot/messageRouter.js
+++ routes/bot/messageRouter.js
@@ -1,4 +1,5 @@
 const { router, text } = require('bottender/router');
+const activityFlex = require('../../template/message/11010_activity_flex.json');
 
 module.exports = async function messageRouter(context) {
     return router([
@@ -6,7 +7,7 @@ module.exports = async function messageRouter(context) {
         text('關於青創', (context)=>{ context.sendText('關於青創'); }),
         text('周邊生活', (context)=>{ context.sendText('周邊生活'); }),
         text('瑞光社群', (context)=>{ context.sendText('瑞光社群'); }),
-        text('活動專區', (context)=>{ context.sendText('活動專區'); }),
-        text('*', (context)=>{ context.sendText('你在工三小'); })
+        text('活動專區', (context)=>{ context.replyFlex("請使用手機查看訊息", activityFlex); }),
+        text('*', (context)=>{ context.sendText('我不曉得您的意思，如果有問題可以私訊我們的臉書專頁 https://fb.ruiguang.link'); })
     ]);
 }
