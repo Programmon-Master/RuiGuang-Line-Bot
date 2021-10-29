fb40d5c51d24b915447fcadecf6b40fd446c08eb
TMineCola <23163771+TMineCola@users.noreply.github.com>
2021-10-29 15:21:45 +0800

    Removed console log

--- routes/web/webRouter.js
+++ routes/web/webRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/qrentry', function(req, res, next) {
-    console.log(req.body);
     res.render('web/activity/welcomePartyTribeScanner', {
         title: '關主掃描器',
         cssList: ['/stylesheets/common/bootstrap.min.css', '/stylesheets/activity/welcomeParty.css'],
