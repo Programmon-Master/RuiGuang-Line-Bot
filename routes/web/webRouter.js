b65a125f406c0c501325b56829211a2a9b11e7c7
TMineCola <23163771+TMineCola@users.noreply.github.com>
2021-11-13 21:34:21 +0800

    Removed QR entry scanner web

--- routes/web/webRouter.js
+++ routes/web/webRouter.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/qrentry', function(req, res, next) {
-    res.render('web/activity/welcomePartyTribeScanner', {
-        title: '關主掃描器',
-        cssList: ['/stylesheets/common/bootstrap.min.css', '/stylesheets/activity/welcomeParty.css'],
-        jsList: ['/javascripts/component/jsQR.min.js', '/javascripts/activity/welcomePartyTribeScanner.js']
-    });
-});
+/**
+ * @deprecated
+ * 
+ * 1030 Welcome party stage leader QR Scanner Service
+ */
+// router.get('/qrentry', function(req, res, next) {
+//     res.render('web/activity/welcomePartyTribeScanner', {
+//         title: '關主掃描器',
+//         cssList: ['/stylesheets/common/bootstrap.min.css', '/stylesheets/activity/welcomeParty.css'],
+//         jsList: ['/javascripts/component/jsQR.min.js', '/javascripts/activity/welcomePartyTribeScanner.js']
+//     });
+// });
 
 module.exports = router;
